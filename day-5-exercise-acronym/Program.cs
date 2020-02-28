328044a78305fe994cdb747a4eaee496a13b051c
Russell Gehan <rdgehan@gmail.com>
2020-02-28 09:14:07 -0600

    Completed acronym exercise.

--- day-5-exercise-acronym/Program.cs
+++ day-5-exercise-acronym/Program.cs
@@ -6,7 +6,25 @@ namespace day_5_exercise_acronym
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myWords = GetInput("Please input a multi-word term: ");
+            PrintAcronym(myWords);
+        }
+        static string GetInput(string msg)
+        {
+            Console.Write(msg);
+            string input = Console.ReadLine();
+            return input;
+        }
+        static void PrintAcronym(string term)
+        {
+            string[] wordsFromTerm = term.Split(" ");
+            string acronym = "";
+            for (int i = 0; i < wordsFromTerm.Length; i++)
+            {
+                acronym += wordsFromTerm[i][0];
+            }
+            acronym = acronym.ToUpper();
+            Console.WriteLine("Your acronym is: " + acronym);
         }
     }
 }
