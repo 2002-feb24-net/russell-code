5708290647fd271087288445177f0af35e46384f
Russell Gehan <rdgehan@gmail.com>
2020-02-28 09:03:52 -0600

    Created project folder for acronym exercise.

---
+++ day-5-exercise-acronym/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day_5_exercise_acronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
