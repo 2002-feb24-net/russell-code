c7d0cdd17148edbf5e8b51dac4b5ce81c5a833e3
Russell Gehan <rdgehan@gmail.com>
2020-03-05 13:34:37 -0600

    Pushing some in-class practices

---
+++ week-B/BubbleSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = new int[20];
+            randomize(arr, 0, 30);
+            Console.WriteLine("Here's my array before sorting: " + string.Join(", ", arr));
+            sort(arr);
+            Console.WriteLine("Here's my array after sorting: " + string.Join(", ", arr));
+        }
+
+        static void sort(int[] a)
+        {
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < a.Length-1-i; j++)
+                {
+                    if(a[j] > a[j+1])
+                    {
+                        swapped = true;
+                        int swap = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = swap;
+                    }
+                }
+                if(!swapped)
+                {
+                    return;
+                }
+            }
+        }
+
+        static void randomize(int[] a, int min, int max)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(min, max);
+            }
+        }
+    }
+}
---
+++ week-B/Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMany = 50;
+            for (int i = 0; i < howMany; i++)
+            {
+                Console.WriteLine("Fib " + i + ": " + Fibonacci(i));
+            }
+        }
+
+        static double Fibonacci(int n)
+        {
+            if(n == 0) return 0;
+            if(n == 1) return 1;
+            double[] values = new double[n+1];
+            values[0] = 0;
+            values[1] = 1;
+            if(n == 1) return 1;
+            for(int i = 2; i < values.Length; i++)
+            {
+                values[i] = values[i-2] + values[i-1];
+            }
+            return values[n];
+        }
+    }
+}
